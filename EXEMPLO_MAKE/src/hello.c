3167349fc2b478643ea76fbdad424804eb45ce55
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-03-18 20:45:42 +0000

    Final files

---
+++ EXEMPLO_MAKE/src/hello.c
@@ -0,0 +1,5 @@
+#include "hello.h"
+
+int  get_id() {
+    return 10; 
+}
---
+++ EXEMPLO_MAKE/src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+int main() {
+    printf("ID= %d!\n", get_id());
+    return 0; 
+}
